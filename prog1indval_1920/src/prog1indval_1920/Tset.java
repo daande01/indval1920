1b8602811efef0e9eaf00092d9f16f7332144bd6
daande01 <daniel.evert.andersson@gmail.com>
2019-09-06 09:16:22 +0200

    start

---
+++ prog1indval_1920/src/prog1indval_1920/Tset.java
@@ -0,0 +1,18 @@
+package prog1indval_1920;
+
+import javax.swing.JOptionPane;
+
+public class Tset {
+
+
+
+		public static void main(String [] arg){
+
+			JOptionPane.showConfirmDialog(null, "hej");
+
+		}
+
+
+
+
+}
