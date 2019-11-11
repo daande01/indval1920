d98e2571800ba840414fcbbeb7d33a06128a1975
daande01 <daniel.evert.andersson@gmail.com>
2019-11-11 16:26:09 +0100

    update

---
+++ prog1indval_1920/src/prog1indval_1920/chartest.java
@@ -0,0 +1,21 @@
+package prog1indval_1920;
+
+public class chartest {
+
+	public static void main(String[] args) {
+
+
+
+	char alpha='a';
+
+	int pos =alpha;
+
+	pos= pos+5;
+
+	char c= (char)pos;
+
+	System.out.println(c);
+
+	}
+
+}
