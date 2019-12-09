390d4424cfe2a6235e1d43953a5063230bc48692
daande01 <daniel.evert.andersson@gmail.com>
2019-12-09 14:50:58 +0100

    nya test filer från lektionen ej fungerande program

---
+++ prog1indval_1920/src/prog1indval_1920/Test1.java
@@ -0,0 +1,56 @@
+package prog1indval_1920;
+
+public class Test1 {
+
+
+
+
+	public static void main(String[] args) {
+
+		int i  ;    // deklarerar variabeln  i av datatypen integer
+
+		double d = 12.0;  // deklarerat variabeln d samt tilldelar den värdet  12.0
+
+		// deklarering + tilldelning = initsiering
+
+		double d1 =Math.pow(2,2);
+
+		char c='A';
+
+
+
+		int chartablenumber=c;
+
+		System.out.println(chartablenumber);
+
+		char cc=(char) chartablenumber;
+
+
+		System.out.println(cc);
+
+
+		boolean  ljuset= false;
+
+		if (ljuset==false && knapp==1) {
+
+			pin1.light(high);
+
+			ljuset=true;
+		}else  if(luset== true && kanpp ==1) {
+
+			pin1.light(LOW);
+		}
+
+
+
+
+
+
+
+	}
+
+
+
+
+
+}
---
+++ prog1indval_1920/src/prog1indval_1920/Test3.java
@@ -0,0 +1,47 @@
+package prog1indval_1920;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Test3 {
+
+
+	public static void main(String[] args) {
+
+
+		int [] i = new int [5];
+
+		 i[0]=10;
+
+		 i[4]=34;
+
+		 System.out.println(Arrays.toString(i));
+
+
+		double [] r = {1,4,5,68,9.0,0,9};
+
+
+		int summa=0;
+
+		Scanner sc = new Scanner(System.in);
+
+
+		System.out.println("ane fem siffror");
+
+		summa=summa+sc.nextInt();
+
+		summa=summa+sc.nextInt();
+
+		summa=summa+sc.nextInt();
+
+		summa=summa+sc.nextInt();
+
+		summa=summa+sc.nextInt();
+
+
+		double medel = summa/5.0;
+
+		System.out.println(medel);
+
+	}
+}
