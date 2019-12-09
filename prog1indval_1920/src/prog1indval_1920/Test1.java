7aa1abf1aea1f5784c0ce380f1c4724f5dd03d36
daande01 <daniel.evert.andersson@gmail.com>
2019-12-09 14:51:28 +0100

    nya test filer från lektionen , ej fungerande program

--- prog1indval_1920/src/prog1indval_1920/Test1.java
+++ prog1indval_1920/src/prog1indval_1920/Test1.java
@@ -2,55 +2,38 @@ package prog1indval_1920;
 
 public class Test1 {
 
-
-
-
 	public static void main(String[] args) {
 
-		int i  ;    // deklarerar variabeln  i av datatypen integer
+		int i; // deklarerar variabeln i av datatypen integer
 
-		double d = 12.0;  // deklarerat variabeln d samt tilldelar den värdet  12.0
+		double d = 12.0; // deklarerat variabeln d samt tilldelar den värdet 12.0
 
 		// deklarering + tilldelning = initsiering
 
-		double d1 =Math.pow(2,2);
+		double d1 = Math.pow(2, 2);
 
-		char c='A';
+		char c = 'A';
 
-
-
-		int chartablenumber=c;
+		int chartablenumber = c;
 
 		System.out.println(chartablenumber);
 
-		char cc=(char) chartablenumber;
-
+		char cc = (char) chartablenumber;
 
 		System.out.println(cc);
 
+		boolean ljuset = false;
 
-		boolean  ljuset= false;
-
-		if (ljuset==false && knapp==1) {
+		if (ljuset == false && knapp == 1) {
 
 			pin1.light(high);
 
-			ljuset=true;
-		}else  if(luset== true && kanpp ==1) {
+			ljuset = true;
+		} else if (luset == true && kanpp == 1) {
 
 			pin1.light(LOW);
 		}
 
-
-
-
-
-
-
 	}
-
-
-
-
 
 }
--- prog1indval_1920/src/prog1indval_1920/Test3.java
+++ prog1indval_1920/src/prog1indval_1920/Test3.java
@@ -5,41 +5,48 @@ import java.util.Scanner;
 
 public class Test3 {
 
-
 	public static void main(String[] args) {
 
+		double k = 10;
+		double u = 3;
 
-		int [] i = new int [5];
+		int svar = k / u;
 
-		 i[0]=10;
+		int rest1 = 10 - (svar * 3);
 
-		 i[4]=34;
+		double rest = k % u;
 
-		 System.out.println(Arrays.toString(i));
+		String[] s = new String[5];
 
+		s[0] = "dsfdgdf dgdfgdfg bdfgdfg";
 
-		double [] r = {1,4,5,68,9.0,0,9};
+		int[] i = new int[5];
 
+		i[0] = 10;
 
-		int summa=0;
+		i[4] = 34;
+
+		System.out.println(Arrays.toString(i));
+
+		double[] r = { 1, 4, 5, 68, 9.0, 0, 9 };
+
+		int summa = 0;
 
 		Scanner sc = new Scanner(System.in);
 
-
 		System.out.println("ane fem siffror");
 
-		summa=summa+sc.nextInt();
+		summa = summa + sc.nextInt();
 
-		summa=summa+sc.nextInt();
+		summa = summa + sc.nextInt();
 
-		summa=summa+sc.nextInt();
+		summa = summa + sc.nextInt();
 
-		summa=summa+sc.nextInt();
+		summa = summa + sc.nextInt();
 
-		summa=summa+sc.nextInt();
+		summa = summa + sc.nextInt();
 
-
-		double medel = summa/5.0;
+		double medel = summa / 5.0;
 
 		System.out.println(medel);
 
