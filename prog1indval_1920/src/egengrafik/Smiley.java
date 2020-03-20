f3033c76881e204243a306916392fe039757e388
daande01 <daniel.evert.andersson@gmail.com>
2020-03-20 11:03:55 +0100

    new file

--- prog1indval_1920/src/egengrafik/Smiley.java
+++ prog1indval_1920/src/egengrafik/Smiley.java
@@ -1,22 +1,68 @@
 package egengrafik;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 
 import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.Timer;
 
 public class Smiley extends JComponent {
 
+	int x = 100;
+	int y = 100;
+	int xv=2;
+	int yv=2;
+
+	static Timer t;
+
+	public Smiley() {
+
+		this.setPreferredSize(new Dimension(400,400));
+
+		t = new Timer(40, e -> {
+
+			update();
+
+		});
+
+		t.start();
+
+	}
+
+	public void update() {
+
+		//yv=yv+1;
+
+
+
+		x += xv;                      /// -20 -19 -18 -1 0 1
+		y = y + yv;
+		repaint();
+
+		if(y>=400-100) {
+			y=300;
+
+			yv=yv*-1;
+
+		}
+
+
+
+
+	}
 
 	@Override
 	protected void paintComponent(Graphics g) {
 
 		super.paintComponent(g);
 		g.setColor(Color.yellow);
-		g.fillOval(100, 100, 100, 100);
+		g.fillOval(x, y, 100, 100);
 		g.setColor(Color.BLACK);
-		g.fillOval(100, 100, 10, 10);
+		g.fillOval(x + 30, y + 30, 10, 10);
+		g.fillOval(x + 60, y + 30, 10, 10);
+		g.drawLine(x + 30, y + 70, x + 70, y + 70);
 
 	}
 
@@ -24,10 +70,9 @@ public class Smiley extends JComponent {
 
 		JFrame f = new JFrame();
 		f.setVisible(true);
-		f.setSize(400,400);
+
 		f.setContentPane(new Smiley());
-
-
+		f.pack();
 
 	}
 
